c80a733c71b48a6259c4570acc49526644823ec7
aka <aaronkaltman@gmail.com>
2023-11-01 23:31:35 -0400

    creating fetch apis

---
+++ lib/api/fetch.ts
@@ -0,0 +1,24 @@
+export{}
+
+import React from "react"
+
+export async function fetchAPI(query: string, { variables }: any = {}) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/graphql`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  })
+
+  const json = await res.json()
+  if (json.errors) {
+    console.error(json.errors)
+    throw new Error("Failed to fetch API")
+  }
+
+  return json.data
+}
